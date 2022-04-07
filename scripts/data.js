b8a26f045d759e762162ad0b4115dce11c868fa1
Tatyana <tatyana@MacBook-Pro-Tatyana.local>
2022-04-07 14:27:53 +0300

    fix: заменить разрешения для файлов картинок, чтобы они показывались на GitHub Pages

--- scripts/data.js
+++ scripts/data.js
@@ -2,26 +2,26 @@
 const initialCards = [
   {
     name: "Архыз",
-    link: "../images/element-arkhyz.jpeg"
+    link: "../images/element-arkhyz.jpg"
   },
   {
     name: "Челябинская область",
-    link: "../images/element-chelyabinsk-oblast.jpeg"
+    link: "../images/element-chelyabinsk-oblast.jpg"
   },
   {
     name: "Иваново",
-    link: "../images/element-ivanovo.jpeg"
+    link: "../images/element-ivanovo.jpg"
   },
   {
     name: "Камчатка",
-    link: "../images/element-kamchatka.jpeg"
+    link: "../images/element-kamchatka.jpg"
   },
   {
     name: "Холмогорский район",
-    link: "../images/element-kholmogorsky-rayon.jpeg"
+    link: "../images/element-kholmogorsky-rayon.jpg"
   },
   {
     name: "Байкал",
-    link: "../images/element-baikal.jpeg"
+    link: "../images/element-baikal.jpg"
   }
 ];
